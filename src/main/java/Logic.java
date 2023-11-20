a0558857429911b53195faff620e7fb22aba7c64
VVVladyslav <138609115+VVVladyslav@users.noreply.github.com>
2023-11-20 23:10:07 +0200

    Update Logic.java

--- src/main/java/Logic.java
+++ src/main/java/Logic.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
 
 public class Logic {
-    Scanner scan = new Scanner(System.in);
     public final char BOX_OF_NUMBERS[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
     public final int[][] WIN_COMB = {
             {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
             {0, 4, 8}, {2, 4, 6}
     };
-    public byte input;
-    public byte rand;
-    public byte zero;
-    public boolean boxAvailable = false;
-    public byte winner = 0;
-    public boolean boxEmptyStatus = false;
     private static final int NUMBER = 9;
-    public boolean result;
-    public boolean isResult;
-    Funct funct = new Funct();
-    Writer writer = new Writer();
-    Winner winner1 = new Winner();
+    private ShowTableGame showTableGame;
+    private ShowMessage showMessage1;
+    private CheckWhoWin checkWhoWin;
+    private byte input;
+    private byte rand;
+    private byte zero;
+    private boolean boxAvailable;
+    private byte winner;
+    private boolean boxEmptyStatus;
+    private boolean result;
+    private boolean isResult;
+    public Logic(CheckWhoWin checkWhoWin, ShowTableGame showTableGame, ShowMessage showMessage1) {
+        boxAvailable = false;
+        winner = 0;
+        boxEmptyStatus = false;
+        this.checkWhoWin = checkWhoWin;
+        this.showTableGame = showTableGame;
+        this.showMessage1 = showMessage1;
+    }
     public void logicInside() {
-
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter box number to select. Enjoy!\n");
 
         while (true) {
 
-            writer.write(BOX_OF_NUMBERS);
+            showTableGame.showTable(BOX_OF_NUMBERS);
 
             if (!boxEmptyStatus) {
                 for (zero = 0; zero < NUMBER; zero++)
@@ -35,7 +41,7 @@ public class Logic {
                     boxEmptyStatus = true;
             }
 
-            isResult = winner1.win(winner);
+            isResult = showMessage1.showToConsole(winner);
 
             if (isResult = false){
                 break;
@@ -44,24 +50,24 @@ public class Logic {
             while (true) {
                 input = scan.nextByte();
                 if (input > 0 && input < 10) {
-                    if (BOX_OF_NUMBERS[input - 1] == 'X' || BOX_OF_NUMBERS[input - 1] == 'O')
+                    if (BOX_OF_NUMBERS[input - 1] == 'X' || BOX_OF_NUMBERS[input - 1] == 'O') {
                         System.out.println("That one is already in use. Enter another.");
-                    else {
+                    } else {
                         BOX_OF_NUMBERS[input - 1] = 'X';
                         break;
                     }
-                } else
+                } else {
                     System.out.println("Invalid input. Enter again.");
+                }
             }
 
-            result = funct.winnerFunctX(WIN_COMB, 'X', BOX_OF_NUMBERS);
+            result = checkWhoWin.check(WIN_COMB, 'X', BOX_OF_NUMBERS);
 
             if (result == true){
                 winner = 1;
             }
 
             boxAvailable = false;
-
 
             for (zero = 0; zero < NUMBER; zero++) {
                 if (BOX_OF_NUMBERS[zero] != 'X' && BOX_OF_NUMBERS[zero] != 'O') {
@@ -83,12 +89,12 @@ public class Logic {
                 }
             }
 
-            result = funct.winnerFunctX(WIN_COMB, 'O', BOX_OF_NUMBERS);
+            result = checkWhoWin.check(WIN_COMB, 'O', BOX_OF_NUMBERS);
 
             if (result == true){
                 winner = 2;
             }
         }
+        scan.close();
     }
-
 }
