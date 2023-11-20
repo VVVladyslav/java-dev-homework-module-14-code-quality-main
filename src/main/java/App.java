88961db3abd88b81235ed9a07ac9e3de3fcf4d46
VVVladyslav <138609115+VVVladyslav@users.noreply.github.com>
2023-11-20 23:09:09 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,12 @@
 public class App {
-
     public static void main(String[] args) {
-        Logic logic = new Logic();
+
+        ShowMessage showMessage1 = new ShowMessage();
+        ShowTableGame showTableGame = new ShowTableGame();
+        CheckWhoWin checkWhoWin = new CheckWhoWin();
+
+        Logic logic = new Logic(checkWhoWin, showTableGame, showMessage1);
         logic.logicInside();
-        }
+
     }
+}
